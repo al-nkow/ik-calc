6054811221a223859108f0759cf83d31524c54e3
Aleksey <aleksey@MacBook-Pro-15.local>
2020-06-19 20:49:56 +0300

    Updates

---
+++ precache-manifest.f04995b3130e30105cc6241474e330b0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd90e7d76d98741106b0665900ed59b4",
+    "url": "/ik-calc/index.html"
+  },
+  {
+    "revision": "813db3de502402b824cb",
+    "url": "/ik-calc/static/css/main.fdf29775.chunk.css"
+  },
+  {
+    "revision": "ded994b619dc06195379",
+    "url": "/ik-calc/static/js/2.163837d5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ik-calc/static/js/2.163837d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "813db3de502402b824cb",
+    "url": "/ik-calc/static/js/main.1aa43d58.chunk.js"
+  },
+  {
+    "revision": "def1428d6e81acad57fa",
+    "url": "/ik-calc/static/js/runtime-main.86da6d78.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ik-calc/static/media/logo.5d5d9eef.svg"
+  }
+]);
